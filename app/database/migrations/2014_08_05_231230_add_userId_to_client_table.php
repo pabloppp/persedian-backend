d36e8f50ad164f57e556753b37e4860caf98c9cc
Pablo Pernias <pablo@pernias.com>
2014-08-12 13:31:20 +0200

    small update

---
+++ app/database/migrations/2014_08_05_231230_add_userId_to_client_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToClientTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+        Schema::table('oauth_clients', function(Blueprint $table)
+        {
+            //
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+        Schema::table('oauth_clients', function(Blueprint $table)
+        {
+            $table->dropForeign('oauth_clients_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+	}
+
+}
