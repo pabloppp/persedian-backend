180924e062af0404d35490f1a6a225b9a38ccbd1
Pablo Pernias <pablo@pernias.com>
2014-08-16 22:17:31 +0200

    Folder organization and refactorization of /public and /app/views

---
+++ public/angular_mainApp/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pablopernias on 16/08/14.
+ */
+
+angular.module('controllerModule', []);
+angular.module('serviceModule', []);
+var commentApp = angular.module('persedianApp', ['controllerModule', 'serviceModule']);
